b5152861780c36cec7b582d9f41065be9ad79078
satyasure <51406383+satyasure@users.noreply.github.com>
2022-08-08 06:56:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,12 +3,3 @@ resource "random_integer" "bucket_id" {
     max = 5
 
 }
-resource "aws_s3_bucket" "tf_backend" {
-    bucket = "${var.project_name}-backend-${random_integer.bucket_id.result}"
-    acl = "private"
-    force_destroy = true
-    tags = {
-        Name = "tf_backend_state"
-    }
-  
-}
